246221b9622cd4ef4b946ecc8c813e9079914c10
Evan Willhite <evan@evanwillhite.com>
2016-09-19 16:18:38 -0500

    gitignore updates and changes to fourk-gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-/* globals require */
+(function () {
+  'use strict';
 
-'use strict';
+  // General
+  var gulp = require('gulp-help')(require('gulp'));
+  var _ = require('lodash');
+  var localConfig = {};
 
-// General
-var gulp = require('gulp-help')(require('gulp'));
-var _ = require('lodash');
-var config = require('./gulp-config');
-var localConfig = {};
+  try {
+    localConfig = require('./local.gulp-config');
+  }
+  catch (e) {}
+  require('fourk-gulp')(gulp, localConfig);
 
-try {
-  localConfig = require('./local.gulp-config');
-}
-catch (e) {}
-config = _.defaultsDeep(localConfig, config);
-require('fourk-gulp')(gulp, config);
+})();
