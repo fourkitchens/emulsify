781be46c8b22badf223a72de9ed75961f5e779da
Evan Willhite <evan@evanwillhite.com>
2016-09-21 15:15:59 -0500

    clean_class filter, trans tag, stuff from build and scripts into package.json

--- .scripts/npm.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# Install Node dependencies
-if [ ! -d node_modules ]
-  then
-    npm install
-fi
--- .scripts/patternlab.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# Prepare the Pattern Lab settings file for installation
-if [ ! -d pattern-lab ]
-  then
-    composer create-project pattern-lab/edition-twig-standard pattern-lab
-    rm -rf pattern-lab/source
-    ln -s ../components pattern-lab/source
-fi
