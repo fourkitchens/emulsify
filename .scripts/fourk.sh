2015e02deee6aef619443dca1a3015f391a0807f
Brian Lewis <brian@modulesunraveled.com>
2016-09-21 09:18:34 -0500

    Changed the way patternlab is installed.

--- .scripts/fourk.sh
+++ .scripts/fourk.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 
 # Prepare the Pattern Lab settings file for installation
-if [ ! -d vendor/pattern-lab/edition-twig-standard/vendor ]
+if [ ! -d pattern-lab ]
   then
-    cd ./vendor/pattern-lab/edition-twig-standard; composer install
-    rm -rf ./config/config.yml
-    ln -s ../../../../pattern-lab-config.yml ./config/config.yml
+    composer create-project pattern-lab/edition-twig-standard pattern-lab
 fi
 
 # Install Node dependencies
