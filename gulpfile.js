4ade9a711e3a60f76eeb1630b0166636e5e4b713
Jeff Tomlinson <jeff@fourkitchens.com>
2017-01-03 15:17:16 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
+/* globals require */
+
 (function () {
   'use strict';
 
   // General
   var gulp = require('gulp-help')(require('gulp'));
-  var _ = require('lodash');
   var localConfig = {};
 
   try {
     localConfig = require('./local.gulp-config');
   }
-  catch (e) {}
+  catch (e) {
+    // Do nothing.
+  }
   require('emulsify-gulp')(gulp, localConfig);
 
 })();
