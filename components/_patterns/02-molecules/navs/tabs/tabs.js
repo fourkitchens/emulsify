e49a50f7532b0a01f6f8a0e27cc20642c171ba0a
Pierre Marcel <pierremarcel@AirForce2.local>
2017-01-05 14:43:24 -0500

    renaming menus to navs

---
+++ components/_patterns/02-molecules/navs/tabs/tabs.js
@@ -0,0 +1,54 @@
+(function () {
+
+  'use strict';
+
+  var el = document.querySelectorAll('.tabs');
+  var tabNavigationLinks = document.querySelectorAll('.tabs__link');
+  var tabContentContainers = document.querySelectorAll('.tabs__tab');
+  var activeIndex = 0;
+
+  /**
+   * handleClick
+   * @description Handles click event listeners on each of the links in the
+   *   tab navigation. Returns nothing.
+   * @param {HTMLElement} link The link to listen for events on
+   * @param {Number} index The index of that link
+   */
+  var handleClick = function (link, index) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      goToTab(index);
+    });
+  };
+
+  /**
+   * goToTab
+   * @description Goes to a specific tab based on index. Returns nothing.
+   * @param {Number} index The index of the tab to go to
+   */
+  var goToTab = function (index) {
+    if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
+      tabNavigationLinks[activeIndex].classList.remove('is-active');
+      tabNavigationLinks[index].classList.add('is-active');
+      tabContentContainers[activeIndex].classList.remove('is-active');
+      tabContentContainers[index].classList.add('is-active');
+      activeIndex = index;
+    }
+  };
+
+  /**
+   * init
+   * @description Initializes the component by removing the no-js class from
+   *   the component, and attaching event listeners to each of the nav items.
+   *   Returns nothing.
+   */
+  for (var e = 0; e < el.length; e++) {
+    el[e].classList.remove('no-js');
+  }
+
+  for (var i = 0; i < tabNavigationLinks.length; i++) {
+    var link = tabNavigationLinks[i];
+    handleClick(link, i);
+  }
+
+})();
