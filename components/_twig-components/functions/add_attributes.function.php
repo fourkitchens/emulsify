9fee0431dec273e0fd99cc8cc2b372330a92c0c2
Jeff Tomlinson <jeff@fourkitchens.com>
2017-08-04 18:55:37 -0600

    Add add_attributes() twig function.

---
+++ components/_twig-components/functions/add_attributes.function.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @file
+ * Add "add_attributes" function for Pattern Lab & Drupal
+ */
+
+use Drupal\Core\Template\Attribute;
+
+$function = new Twig_SimpleFunction('add_attributes', function ($context, $additional_attributes = []) {
+  if (class_exists('Drupal')) {
+    $attributes = new Attribute();
+
+    if (!empty($additional_attributes)) {
+      foreach ($additional_attributes as $key => $value) {
+        if (!is_array($value)) {
+          if (is_string($value)) {
+            $value = [$value];
+          }
+          else {
+            continue;
+          }
+        }
+        // Merge additional attribute values with existing ones.
+        if ($context['attributes']->offsetExists($key)) {
+          $existing_attribute = $context['attributes']->offsetGet($key)->value();
+          $value = array_merge($existing_attribute, $value);
+        }
+
+        $context['attributes']->setAttribute($key, $value);
+      }
+    }
+
+    // Set all attributes.
+    foreach($context['attributes'] as $key => $value) {
+      $attributes->setAttribute($key, $value);
+      // Remove this attribute from context so it doesn't filter down to child
+      // elements.
+      $context['attributes']->removeAttribute($key);
+    }
+
+    return $attributes;
+  }
+  else {
+    $attributes = [];
+
+    foreach ($additional_attributes as $key => $value) {
+      if (is_array($value)) {
+        $attributes[] = $key . '="' . implode(' ', $value) . '"';
+      }
+      else {
+        $attributes[] = $key . '="' . $value . '"';
+      }
+    }
+
+    return implode(' ', $attributes);
+  }
+
+}, array('needs_context' => true, 'is_safe' => array('html')));
