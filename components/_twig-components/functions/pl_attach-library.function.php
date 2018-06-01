040b3f792b41f0b1d1e69b6c2bb2e66eb4a2a65f
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-06-01 14:51:24 -0500

    removed consoling, added libraries

--- components/_twig-components/functions/pl_attach-library.function.php
+++ components/_twig-components/functions/pl_attach-library.function.php
@@ -4,15 +4,15 @@
  * Add "attach_library" function for Pattern Lab.
  */
 
-$function = new Twig_SimpleFunction('attach_library', function ($string) {
+$function = new Twig_SimpleFunction('attach_library', function ($context, $string) {
   // Get Library Name.
   $libraryName = substr($string, strpos($string, "/") + 1);
-  $files = glob('dist/*/' . $libraryName . '/*.js');
+  $files = glob('dist/*/' . $libraryName . '/' . $libraryName . '*.js');
   foreach($files as $jsPath) {
     $data = array("hello" => "world");
-    $scriptString = '<script src="/' . $jsPath . '"></script>';
+    $scriptString = '<script async src="/' . $jsPath . '"></script>';
     $stringLoader = \PatternLab\Template::getStringLoader();
     $output = $stringLoader->render(array("string" => $scriptString, "data" => $data));
     return $output;
 	}
-});
+}, array('needs_context' => true, 'is_safe' => array('html')));
