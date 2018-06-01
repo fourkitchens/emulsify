be53bf07a6caafac47dc95ef18aa5cd00f943209
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-06-01 14:11:27 -0500

    65: attach_library function

--- components/_twig-components/functions/pl_attach-library.function.php
+++ components/_twig-components/functions/pl_attach-library.function.php
@@ -4,6 +4,15 @@
  * Add "attach_library" function for Pattern Lab.
  */
 
-$function = new Twig_SimpleFunction('attach_library', function ($string) {
-  return $string;
-});
+$function = new Twig_SimpleFunction('attach_library', function ($context, $string) {
+  // Get Library Name.
+  $libraryName = substr($string, strpos($string, "/") + 1);
+  $files = glob('dist/*/' . $libraryName . '/*.js');
+  foreach($files as $jsPath) {
+    $data = array("hello" => "world");
+    $scriptString = '<script src="/' . $jsPath . '"></script>';
+    $stringLoader = \PatternLab\Template::getStringLoader();
+    $output = $stringLoader->render(array("string" => $scriptString, "data" => $data));
+    return $output;
+	}
+}, array('needs_context' => true, 'is_safe' => array('html')));
