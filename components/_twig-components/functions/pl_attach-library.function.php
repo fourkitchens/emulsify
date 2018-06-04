c415dfdce5e629f634563efa136fc14002b9dba6
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-06-04 13:22:25 -0500

    removed unnecessary context, added comments

--- components/_twig-components/functions/pl_attach-library.function.php
+++ components/_twig-components/functions/pl_attach-library.function.php
@@ -4,17 +4,21 @@
  * Add "attach_library" function for Pattern Lab.
  */
 
-$function = new Twig_SimpleFunction('attach_library', function ($context, $string) {
-  // Get Library Name.
+$function = new Twig_SimpleFunction('attach_library', function ($string) {
+  // Get Library Name from string.
   $libraryName = substr($string, strpos($string, "/") + 1);
+
+  // Search dist root directory for library.
   $files = glob('dist/*/' . $libraryName . '/' . $libraryName . '*.js');
+  // Search dist nested directory for library.
   if (empty($files)) {
     $files = glob('dist/*/*/' . $libraryName . '/' . $libraryName . '*.js');
   }
+  // For each file, create an async script to insert to the Twig component.
   foreach($files as $jsPath) {
     $scriptString = '<script async src="/' . $jsPath . '"></script>';
     $stringLoader = \PatternLab\Template::getStringLoader();
     $output = $stringLoader->render(array("string" => $scriptString, "data" => []));
     return $output;
 	}
-}, array('needs_context' => true, 'is_safe' => array('html')));
+}, array('is_safe' => array('html')));
