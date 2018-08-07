84adc7307b928a486b81c6322c70c47463ebd999
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-08-07 12:11:23 -0500

    65: rewrite to parse libraries.yml file

--- components/_twig-components/functions/pl_attach-library.function.php
+++ components/_twig-components/functions/pl_attach-library.function.php
@@ -4,21 +4,29 @@
  * Add "attach_library" function for Pattern Lab.
  */
 
+use Symfony\Component\Yaml\Yaml;
+
 $function = new Twig_SimpleFunction('attach_library', function ($string) {
   // Get Library Name from string.
   $libraryName = substr($string, strpos($string, "/") + 1);
 
-  // Search dist root directory for library.
-  $files = glob('dist/*/' . $libraryName . '/*.js');
-  // Search dist nested directory for library.
-  if (empty($files)) {
-    $files = glob('dist/*/*/' . $libraryName . '/*.js');
+  // Find Library in libraries.yml file.
+  $yamlFile = glob('*.libraries.yml');
+  $yamlOutput = Yaml::parseFile($yamlFile[0]);
+
+  // For each item in .libraries.yml file.
+  foreach($yamlOutput as $key => $value) {
+
+    // If the library exists.
+    if ($key === $libraryName) {
+      $files = $yamlOutput[$key]['js'];
+      // For each file, create an async script to insert to the Twig component.
+      foreach($files as $key => $jsPath) {
+        $scriptString = '<script async src="/' . $key . '"></script>';
+        $stringLoader = \PatternLab\Template::getStringLoader();
+        $output = $stringLoader->render(array("string" => $scriptString, "data" => []));
+        return $output;
+    	}
+    }
   }
-  // For each file, create an async script to insert to the Twig component.
-  foreach($files as $jsPath) {
-    $scriptString = '<script async src="/' . $jsPath . '"></script>';
-    $stringLoader = \PatternLab\Template::getStringLoader();
-    $output = $stringLoader->render(array("string" => $scriptString, "data" => []));
-    return $output;
-	}
 }, array('is_safe' => array('html')));
