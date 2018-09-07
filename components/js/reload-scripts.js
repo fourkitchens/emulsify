290a193e9a2318c3f7c0aa57af9c6727954b21eb
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-09-07 09:50:16 -0500

    separated script, commented

---
+++ components/js/reload-scripts.js
@@ -0,0 +1,21 @@
+/*
+* Reload Scripts on page (required for attach_library)
+*
+*/
+
+// Get body tag.
+const head = document.getElementsByTagName('body')[0];
+// Get scripts within the body.
+const scripts = head.querySelectorAll('script');
+
+scripts.forEach((element) => {
+  // If the script has the data-name attribute.
+  if (element.dataset.name) {
+    // Create new script element.
+    const script = document.createElement('script');
+    // Set src to script above.
+    script.src = element.src;
+    // Append to head.
+    head.appendChild(script);
+  }
+});
