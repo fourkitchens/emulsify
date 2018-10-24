ecce9a64a8ee22318a86c286a6e29d2f342c7c0a
Randy Oest <oest@amazingrando.com>
2018-10-24 09:56:39 -0400

    Replacing bash scripts with node scripts

---
+++ scripts/node/pattern_lab.js
@@ -0,0 +1,11 @@
+/**
+ * Install Pattern Lab.
+ */
+const shell = require("shelljs");
+
+shell.echo("Let's install PATTERN LAB.");
+shell.rm('-rf', 'pattern-lab');
+shell.exec('composer create-project -n pattern-lab/edition-twig-standard pattern-lab')
+shell.rm('-rf', 'pattern-lab/source');
+shell.ln('-s', '../components', 'pattern-lab/source')
+shell.exit();
---
+++ scripts/node/twig_functions.js
@@ -0,0 +1,37 @@
+/**
+ * Install Pattern Lab.
+ */
+const shell = require("shelljs");
+const fs = require('fs');
+const path = {
+  'drupalComposer': '../../../../vendor/drupal-pattern-lab',
+  'drupalNoComposer': './../../vendor/drupal-pattern-lab',
+  'standalone': 'vendor/drupal-pattern-lab'
+}
+
+function vendorPathInstalled() {
+  if (fs.existsSync(path.standalone) || fs.existsSync(path.drupalComposer)  || fs.existsSync(path.drupalNoComposer)) {
+    return true;
+  }
+  return false;
+}
+
+function vendorDirectoryRoute() {
+  if (fs.existsSync(path.standalone)) {
+    return path.standalone;
+  } else {
+    return "foo"
+  }
+}
+
+if (vendorPathInstalled()) {
+  const vendorPath = vendorDirectoryRoute();
+  shell.echo('✅ Vendor directory found. Installing Twig Functions.')
+  shell.cp(`${vendorPath}/add-attributes-twig-extension/add_attributes.function.php`, 'components/_twig-components/functions/');
+  shell.cp(`${vendorPath}/bem-twig-extension/bem.function.php`, 'components/_twig-components/functions/');
+  shell.cp(`${vendorPath}/attach-library-twig-extension/pl_attach-library.function.php`, 'components/_twig-components/functions/');
+} else {
+  shell.echo('🚫 Vendor directory not found.')
+  shell.echo('💻 Please run `composer install` and run this again.')
+}
+shell.exit();
