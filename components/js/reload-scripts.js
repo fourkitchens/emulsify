3454729cfe0587133b03a8e8d5d08f6106084368
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-11-27 11:04:09 -0600

    script with callback loading

--- components/js/reload-scripts.js
+++ components/js/reload-scripts.js
@@ -2,20 +2,34 @@
 * Reload Scripts on page (required for attach_library)
 *
 */
-
+const d = document;
 // Get body tag.
-const head = document.getElementsByTagName('body')[0];
+const head = d.getElementsByTagName('body')[0];
 // Get scripts within the body.
 const scripts = head.querySelectorAll('script');
 
-scripts.forEach((element) => {
+function async(src, callback) {
+  const script = document.createElement('script');
+  script.src = src;
+  if (callback) {
+    script.addEventListener(
+      "load",
+      e => {
+        callback(null, e);
+      },
+      false
+    );
+  }
+  head.appendChild(script);
+}
+
+scripts.forEach(element => {
   // If the script has the data-name attribute.
   if (element.dataset.name) {
-    // Create new script element.
-    const script = document.createElement('script');
-    // Set src to script above.
-    script.src = element.dataset.src;
-    // Append to head.
-    head.appendChild(script);
+    async(element.dataset.src, () => {
+      if (typeof Drupal === 'object' && typeof Drupal.attachBehaviors === 'function') {
+        Drupal.attachBehaviors();
+      }
+    });
   }
 });
