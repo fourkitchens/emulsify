f0371d0ed78c90b51055d6446bcce22caedf7854
Evan Willhite <evan@fourkitchens.com>
2019-02-04 18:02:40 -0600

    115: turned off babel debugging

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,7 @@ module.exports = (api) => {
   const presets = [
     [
       '@babel/preset-env', {
-        useBuiltIns: 'entry',
-        debug: true
+        useBuiltIns: 'entry'
       },
     ],
     'minify',
